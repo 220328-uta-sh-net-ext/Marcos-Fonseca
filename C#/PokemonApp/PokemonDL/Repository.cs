7408ac32021095b1e10801f248d72ebd161e81a9
Markfon <m4rkfons@outlook.com>
2022-04-18 16:56:19 -0400

    Pokemon JSON

---
+++ C#/PokemonApp/PokemonDL/Repository.cs
@@ -0,0 +1,26 @@
+﻿using PokemonModels;
+using System.Collections.Generic;
+using System.IO;
+namespace PokemonDL
+{
+    public class Repository : IRepository
+    {
+        private string filePath = "../PokemonDL/Database";
+        private string jsonString;
+
+        public Pokemon addPokemon(Pokemon poke)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetAllPokemon()
+        {
+            jsonString = File.ReadAllText(filePath+"Pokemon.json");
+        }
+
+        List<Pokemon> IRepository.GetAllPokemon()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ C#/PokemonApp/PokemonUI/PokemonOperations.cs
@@ -0,0 +1,16 @@
+﻿using PokemonDL;
+
+namespace PokemonUI
+{
+    internal class PokemonOperations
+    {
+        Repository repository= new Repository();
+
+        public void GetAllPokemons() {
+            repository.GetAllPokemons();
+    }
+        public static void AddDummyPokemon()
+        {
+            PokemonDL pokemon1 = new Pokemon();    
+        }
+}
