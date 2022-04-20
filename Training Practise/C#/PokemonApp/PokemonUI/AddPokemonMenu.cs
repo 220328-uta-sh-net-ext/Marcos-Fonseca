f631e565779addb1d465aaad52b7a1171478c804
Marcos Fonseca <40308538+Markfon@users.noreply.github.com>
2022-04-19 21:45:12 -0400

    Update AddPokemonMenu.cs

--- Training Practise/C#/PokemonApp/PokemonUI/AddPokemonMenu.cs
+++ Training Practise/C#/PokemonApp/PokemonUI/AddPokemonMenu.cs
@@ -3,15 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using PokemonDL;
 using PokemonModels;
 namespace PokemonUI
 {
     internal class AddPokemonMenu : IMenu
     {
+        //static non-access modifier is needed to keep this variable consistent to all objects we create out of our AddPokeMenu
         private static Pokemon newPokemon = new Pokemon();
+
+        private IRepository _repository = new Repository(); //UpCasting
         public void Display()
         {
             Console.WriteLine("Enter Pokemon Information");
+            //feel free to add more options here like for Defense, Health etc....
             Console.WriteLine("<3> Name - " + newPokemon.Name);
             Console.WriteLine("<2> Level - " + newPokemon.Level);
             Console.WriteLine("<1> Save");
@@ -25,6 +30,19 @@ namespace PokemonUI
             {
                 case "0":
                     return "MainMenu";
+                case "1":
+                    _repository.AddPokemon(newPokemon);
+                    Console.WriteLine("----Pokemon Added----");
+                    return "MainMenu";
+                case"2":
+                    Console.Write("Please enter a level ");
+                    newPokemon.Level = Convert.ToInt32(Console.ReadLine());
+                    return "AddPokemon";
+                case"3":
+                    Console.Write("Please enter a name! ");
+                    newPokemon.Name = Console.ReadLine();
+                    return "AddPokemon";
+                /// Add more cases for any other attributes of pokemon
                 default:
                     Console.WriteLine("Please input a valid response");
                     Console.WriteLine("Please press Enter to continue");
