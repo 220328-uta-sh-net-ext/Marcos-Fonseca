5cf0888fecd850870c1a7ab563bebd2c47d71b23
Markfon <m4rkfons@outlook.com>
2022-05-08 22:05:40 -0400

    Multicast Delegates

---
+++ Training Practise/Service/MulticastDelegates/MulticastDelegates/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MulticastDelegates
+{
+    public delegate void DogSound(int repeat);
+    
+    public class DogSoundDelegate
+    {
+        static void Main(string[] args)
+        {
+            DogSound dg = null;
+
+
+            dg += new DogSound(Rage);
+            dg += new DogSound(Happy);
+            dg += new DogSound(Hungry);
+            dg += new DogSound(Thirst);
+
+            if (args.Length == 1) dg(Int32.Parse(args[0]));
+            else dg(10);
+        }
+
+        static void Rage (int repeat)
+        {
+            for (int i = 0; i < repeat; i++)
+                Console.WriteLine("Grrrrrrrrrrrrrrrrrrrrrrr");
+        }
+        static void Happy(int repeat)
+        {
+            for (int i = 0; i < repeat; i++)
+                Console.WriteLine("Au Au Au Au Au Au Au Au!!!");
+        }
+        static void Hungry(int repeat)
+        {
+            for (int i = 0; i < repeat; i++)
+                Console.WriteLine("Auuuuuuuuuuuuuuuuuuuuuuu");
+        }
+        static void Thirst(int repeat)
+        {
+            for (int i = 0; i < repeat; i++)
+                Console.WriteLine("Hmmmm Hmmmm Hmmmm");
+                Console.ReadLine();
+        }
+    }
+}
