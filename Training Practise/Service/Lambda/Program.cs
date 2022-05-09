6de3b3d6209ddd9c54bad1d237567c7de2ee388c
Markfon <m4rkfons@outlook.com>
2022-05-09 10:45:38 -0400

    Lambda Expression

---
+++ Training Practise/Service/Lambda/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lambda
+{
+    public class Program
+    {
+        delegate bool IsTeenAger(Student stud);
+
+        public static void Main()
+        {
+            string userInput;
+            int intVal;
+
+            Console.Write("Are you a teenager? How old are you?  ");
+            userInput = Console.ReadLine();
+            intVal = Convert.ToInt32(userInput);
+
+            //Anonymous Method
+            IsTeenAger isTeenAger = delegate (Student s) { return s.Age > 12 && s.Age < 20; };
+
+             Student stud = new Student() { Age = intVal };
+
+             Console.WriteLine(isTeenAger(stud));
+             Console.ReadLine();
+
+            //Lambda Expression
+            //The Lambda expression evolves from anonymous method by first removing the delegate keyword and parameter type and adding a lambda operator
+            //s is a parameter
+            //=> is the lambda operator
+            //s.Age > 12 && s.Age < 20 is the body expression
+
+            /*IsTeenAger isTeenAger = s => s.Age > 12 && s.Age < 20;
+
+            Student stud = new Student() { Age = intVal };
+
+            Console.WriteLine(isTeenAger(stud));
+            Console.ReadLine();*/
+        }
+    }
+    public class Student
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
